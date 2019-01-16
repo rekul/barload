f1e10c16edf390d4e3bdad4db8652d4a50744656
rekul <haydnluker@gmail.com>
2019-01-15 22:08:51 -0600

    revisions

--- script.js
+++ script.js
@@ -1,212 +1,247 @@
-$( document ).ready(function() {
-	var collars = false;
-	var kgs = true;
-	$('#big-increase').click(function(){
-		console.log("big increase clicked");
-		changeLoad(5);
-	});
+$(document).ready(function() {
+	//settings
+    var collars = false;
+    var kgs = true;
+    var kgBar = true;
 
-	$('#increase').click(function(){
-		console.log("increase clicked");
-		changeLoad(0.5);
-	});
+    //animation values
+    var currentOffset = 0;
+    var currentPlate = 0;
+    var currentDelay = 0;
 
-	$('#decrease').click(function(){
-		console.log("decrease clicked");
-		changeLoad(-0.5);
-	});
+    //init
+    changeLoad(0);
 
-	$('#big-decrease').click(function(){
-		console.log("big-decrease clicked");
-		changeLoad(-5);
-	});
+    $('#increase').click(function() {
+        console.log("increase clicked");
+        changeLoad(2.5);
+    });
 
-	$('#radio-kg').click(function(){
-		console.log("kg clicked");
-		if($('.unit-display').text() == 'lb'){
-			$('.weight-display').text(Math.round(2*(1/2.205)*parseFloat($('.weight-display').text()))/2);
-		}
-		$('.unit-display').text("kg");
-		kgs = true;
-		changeLoad(0);
-	});
+    $('#decrease').click(function() {
+        console.log("decrease clicked");
+        changeLoad(-2.5);
+    });
 
-	$('#radio-lb').click(function(){
-		console.log("lb clicked");
-		if($('.unit-display').text() == 'kg'){
-			$('.weight-display').text(Math.round(2*(2.205)*parseFloat($('.weight-display').text()))/2);
-		}
-		$('.unit-display').text("lb");
-		kgs = false;
-		changeLoad(0);
-	});
-	$('#collars').change(function(){
-		collars = this.checked;
-		changeLoad(0);
-	});
+    $('.weight-display').change(function() {
+        changeLoad(0);
+    });
 
-	function changeLoad(weightChange){
-		let currentWeight = $('.weight-display').text();
-		currentWeight = parseFloat(currentWeight) + parseFloat(weightChange);
-		$('.weight-display').text(currentWeight);
+    $('#radio-kg').click(function() {
+        console.log("kg clicked");
+        if ($('.unit-display').text() == 'lb') {
+            $('.weight-display').val(Math.round(2 * (1 / 2.205) * parseFloat($('.weight-display').val())) / 2);
+        }
+        $('.unit-display').text("kg");
+        kgs = true;
+        changeLoad(0);
+    });
 
-		//animate plates
-		resetAnimationContainer();
-		if($('.unit-display').text() == 'lb'){
-			currentWeight *= (1/2.205);
-		}
-		let idealWeight = currentWeight;
-		//subtract barbell
-		currentWeight -= (45 * (1/2.205));
-		//if collars, subtract 5kg
-		if(collars){
-			currentWeight -= 5.0;
-		}
-		var error = loadPlates(currentWeight);
-		if(kgs){
-			$('.actual-load').text("Actual Load: " + (Math.round(10*(idealWeight - error))/10) + "kg (-" + Math.round(10*error)/10 + "kg)");
-		}else{
-			$('.actual-load').text("Actual Load: " + (Math.round(2.205*10*(idealWeight - error))/10) + "lbs (-" + Math.round(10*error*(2.205))/10 + "lbs)");	
-		}
-		if(collars){
-			$('.plate-text').text($('.plate-text').text() + " 2.5kg collars ");
-			loadCollars();
-		}
-	}
+    $('#radio-lb').click(function() {
+        console.log("lb clicked");
+        if ($('.unit-display').text() == 'kg') {
+            $('.weight-display').val(Math.round(2 * (2.205) * parseFloat($('.weight-display').val())) / 2);
+        }
+        $('.unit-display').text("lb");
+        kgs = false;
+        changeLoad(0);
+    });
 
-	function resetAnimationContainer(){
-		$('.animation-container').empty();
-		currentOffset = 0;
-		currentPlate = 0;
-		currentDelay = 0;
-	}
+    $('#collars').change(function() {
+        collars = this.checked;
+        changeLoad(0);
+    });
 
-	function loadPlates(load){
-		$('.plate-text').empty();
-		return loadReds(load);
-	}
+    $('#kg-bar').change(function() {
+        kgBar = this.checked;
+        changeLoad(0);
+    });
 
-	function addPlateText(number, text){
-		if(number === 0){
-			return;
-		}
-		if(number === 1){
-			$('.plate-text').text($('.plate-text').text() + number + " " + text + " ");
-			return true;
-		}
-		if(number > 1){
-			$('.plate-text').text($('.plate-text').text() + number + " " + text + "s ");
-			return true;
-		}
-	}
-	function loadCollars(){
-		if(collars){
-			var plates = ["<div class='collar' id='plate"+currentPlate+"'></div>"].join('');
-			$('.animation-container').append(plates);
-			TweenMax.fromTo($('#plate'+currentPlate), 0.3, {x:(currentOffset+180), scale:0, ease:Power3.easeOut}, {scale:0.5,x:currentOffset, delay:currentDelay});
-		}
-	}
-	function loadReds(load){
-		var plates = 0;
-		while(load > 50){
-			plates++;
-			load-=50;
-		}
-		addWeight('red', plates, 1);
-		return loadBlues(load);
-	}
+    function roundToTen(number){
+    	return Math.round(10*number)/10;
+    }
+    function changeLoad(weightChange) {
+        resetAnimationContainer();
 
-	function loadBlues(load){
-		var plates = 0;
-		while(load > 40){
-			plates++;
-			load-=40;
-		}
-		addWeight('blue', plates, 1);
-		return loadGreens(load);
-	}
+        //get & update displayed weight
+        let currentWeight = $('.weight-display').val();
+        currentWeight =	roundToTen(parseFloat(currentWeight) + parseFloat(weightChange));
+        $('.weight-display').val(currentWeight);
 
-	function loadGreens(load){
-		var plates = 0;
-		while(load > 20){
-			plates++;
-			load-=20;
-		}
-		addWeight('green', plates, 1);
-		return loadWhites(load);
-	}
+        //convert to lbs if displaying lbs
+        if ($('.unit-display').text() == 'lb') {
+            currentWeight *= (1 / 2.205);
+            currentWeight = roundToTen(currentWeight);
+        }
 
-	function loadWhites(load){
-		var plates = 0;
-		while(load > 10){
-			plates++;
-			load-=10;
-		}
-		addWeight('white', plates, 1);
-		return loadFives(load);
-	}
+        //store ideal weight for later
+        let idealWeight = currentWeight;
 
-	function loadFives(load){
-		var plates = 0;
-		if(load > 4.54){
-			plates++;
-			load-=4.54;
-		}
-		addWeight('fivelb', plates, 0.5);
-		return loadTwos(load);
-	}
+        //subtract barbell weight
+        if (kgBar) {
+            currentWeight -= 20;
+        } else {
+            currentWeight -= (45 * (1 / 2.205));
+        }
 
-	function loadTwos(load){
-		var plates = 0;
-		if(load > 2.27){
-			plates++;
-			load-=2.27;
-		}
-		addWeight('twolb', plates, 0.5);
-		return loadOnes(load);
-	}
+        //if collars, subtract 5kg collar weight, add them last
+        if (collars) {
+            currentWeight -= 5.0;
+        }
 
-	function loadOnes(load){
-		var plates = 0;
-		if(load > 1.13){
-			plates++;
-			load-=1.13;
-		}
-		addWeight('onelb', plates, 0.5);
-		return load;
-	}
+        //calculate and display exact load & error
+        var error = loadPlates(currentWeight);
+        if (kgs) {
+            $('.actual-load').text("Actual Load: " + roundToTen(idealWeight - error) + "kg (-" + roundToTen(error) + "kg)");
+        } else {
+            $('.actual-load').text("Actual Load: " + roundToTen((idealWeight - error) * 2.205) + "lbs (-" + roundToTen(error * 2.205) + "lbs)");
+        }
+
+        //add collars last
+        if (collars) {
+            $('.plate-text').text($('.plate-text').text() + " 2.5kg collars ");
+            loadCollars();
+        }
+    }
+
+    function resetAnimationContainer() {
+        $('.animation-container').empty();
+        currentOffset = 0;
+        currentPlate = 0;
+        currentDelay = 0;
+    }
+
+    function loadPlates(load) {
+        $('.plate-text').empty();
+        return loadReds(load);
+    }
+
+    function addPlateText(number, text) {
+        if (number === 0) {
+            return;
+        }
+        if (number === 1) {
+            $('.plate-text').text($('.plate-text').text() + number + " " + text + " ");
+            return true;
+        }
+        if (number > 1) {
+            $('.plate-text').text($('.plate-text').text() + number + " " + text + "s ");
+            return true;
+        }
+    }
+
+    function loadCollars() {
+        if (collars) {
+            var plates = ["<div class='collar' id='plate" + currentPlate + "'></div>"].join('');
+            $('.animation-container').append(plates);
+            TweenMax.fromTo($('#plate' + currentPlate), 0.3, {
+                x: (currentOffset + 180),
+                scale: 0,
+                ease: Power3.easeOut
+            }, {
+                scale: 0.5,
+                x: currentOffset,
+                delay: currentDelay
+            });
+        }
+    }
+
+    function loadReds(load) {
+        var plates = 0;
+        while (load >= 50) {
+            plates++;
+            load -= 50;
+        }
+        addWeight('red', plates, 1);
+        return loadBlues(load);
+    }
+
+    function loadBlues(load) {
+        var plates = 0;
+        while (load >= 40) {
+            plates++;
+            load -= 40;
+        }
+        addWeight('blue', plates, 1);
+        return loadGreens(load);
+    }
+
+    function loadGreens(load) {
+        var plates = 0;
+        while (load >= 20) {
+            plates++;
+            load -= 20;
+        }
+        addWeight('green', plates, 1);
+        return loadWhites(load);
+    }
+
+    function loadWhites(load) {
+        var plates = 0;
+        while (load >= 10) {
+            plates++;
+            load -= 10;
+        }
+        addWeight('white', plates, 0.8);
+        return loadFives(load);
+    }
+
+    function loadFives(load) {
+        var plates = 0;
+        if (load >= 4.54) {
+            plates++;
+            load -= 4.54;
+        }
+        addWeight('fivelb', plates, 0.5);
+        return loadTwos(load);
+    }
+
+    function loadTwos(load) {
+        var plates = 0;
+        if (load >= 2.27) {
+            plates++;
+            load -= 2.27;
+        }
+        addWeight('twolb', plates, 0.5);
+        return loadOnes(load);
+    }
+
+    function loadOnes(load) {
+        var plates = 0;
+        if (load >= 1.13) {
+            plates++;
+            load -= 1.13;
+        }
+        addWeight('onelb', plates, 0.5);
+        return load;
+    }
 
 
 
+    function addWeight(plate, howMany, thin) {
+        if (howMany > 0) {
+            addPlateText(howMany, plate);
+            var added = 0;
 
+            while (added < howMany) {
+                console.log("addweight(" + plate + ");")
+                var plates = ["<div class='" + plate + "' id='plate" + currentPlate + "'></div>"].join('');
+                $('.animation-container').append(plates);
+                TweenMax.fromTo($('#plate' + currentPlate), 0.3, {
+                    x: (currentOffset + 200),
+                    scale: 0,
+                    ease: Power3.easeOut
+                }, {
+                    x: currentOffset,
+                    scale: 0.5,
+                    delay: currentDelay
+                });
+                currentPlate++;
+                currentOffset += 16 * thin;
+                added++;
+                currentDelay += 0.25;
+            }
 
-
-function updateIdealWeightLb(weight){
-	updateIdealWeight(weight * 0.453592);
-}
-
-var currentOffset = 0;
-var currentPlate = 0;
-var currentDelay = 0;
-
-function addWeight(plate, howMany, thin){
-	if(howMany > 0){
-		addPlateText(howMany, plate);
-		var added = 0;
-		
-		while(added < howMany){
-			console.log("addweight(" + plate + ");")
-			var plates = ["<div class='"+plate+"' id='plate"+currentPlate+"'></div>"].join('');
-			$('.animation-container').append(plates);
-			TweenMax.fromTo($('#plate'+currentPlate), 0.3, {x:(currentOffset+200), scale:0, ease:Power3.easeOut}, {scale:0.5,x:currentOffset, delay:currentDelay});
-			currentPlate++;
-			currentOffset+=16*thin;
-			added++;
-			currentDelay+=0.15;
-		}
-
-	}	
-}
+        }
+    }
 
 });
-// changeLoad(0);
